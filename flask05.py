c34cc3ee2bd0249f8002b228d8f0d5a2c56c8042
Kelsey Locaylocay <klocaylo@uncc.edu>
2020-12-02 19:32:59 -0500

    adding and displaying comments functionality

--- flask05.py
+++ flask05.py
@@ -14,6 +14,8 @@ from forms import RegisterForm
 import bcrypt
 from flask import session
 from forms import LoginForm
+from models import Comment as Comment
+from forms import RegisterForm, LoginForm, CommentForm
 
 
 app = Flask(__name__)     # create an app
@@ -59,9 +61,18 @@ def get_notes():
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = db.session.query(User).filter_by(email='boxendi3@uncc.edu').one()
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
-    return render_template('note.html', note=my_note, user = a_user)
+    #check if a user is saved in session
+    if session.get('user'):
+
+        #retrieve note from database
+        my_note = db.session.query(Note).filter_by(id=note_id, user_id=session['user_id']).one()
+
+        # create a comment form object
+        form = CommentForm()
+
+        return render_template('note.html', note = my_note, user=session['user'], form=form)
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
@@ -205,6 +216,23 @@ def logout():
         session.clear()
 
     return redirect(url_for('index'))
+
+@app.route('/notes/<note_id>/comment', methods=['POST'])
+def new_comment(note_id):
+    if session.get('user'):
+        comment_form = CommentForm()
+        # validate_on_submit only validates using POST
+        if comment_form.validate_on_submit():
+            # get comment data
+            comment_text = request.form['comment']
+            new_record = Comment(comment_text, int(note_id), session['user_id'])
+            db.session.add(new_record)
+            db.session.commit()
+
+        return redirect(url_for('get_note', note_id=note_id))
+
+    else:
+        return redirect(url_for('login'))
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
