4df2027341f7d97c3538201846028d57c583d755
Kelsey Locaylocay <klocaylo@uncc.edu>
2020-12-14 21:34:37 -0500

    changed note title and added todo features

--- models.py
+++ models.py
@@ -2,12 +2,14 @@ import os
 from database import db
 import datetime
 
+
+# class that maos to the user and note tables in our database
 class Note(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
     title = db.Column("title", db.String(200))
     text = db.Column("text", db.String(100))
     date = db.Column("date", db.String(50))
-
+    comments = db.relationship("Comment", backref="note", cascade="all, delete-orphan", lazy=True)
 
     # can create a foreign key; referencing the id variable in the User class,
     # so that is why it is lowercase u
@@ -28,6 +30,7 @@ class User(db.Model):
     password = db.Column(db.String(255), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
     notes = db.relationship("Note", backref="note", lazy=True)
+    comments = db.relationship("Comment", backref="user", lazy=True)
 
     def __init__(self, first_name, last_name, email, password):
         self.first_name = first_name
@@ -35,3 +38,33 @@ class User(db.Model):
         self.email = email
         self.password = password
         self.registered_on = datetime.date.today()
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False)
+    content = db.Column(db.VARCHAR, nullable=False)
+    note_id = db.Column(db.Integer, db.ForeignKey("note.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __init__(self, content, note_id, user_id):
+        self.date_posted = datetime.date.today()
+        self.content = content
+        self.note_id = note_id
+        self.user_id = user_id
+
+# class that maps to the user and todo tables in our database
+class Todo(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    title = db.Column("title", db.String(200))
+    date = db.Column("date", db.String(50))
+    checks = db.Column("checks", db.Boolean(False))
+
+    # can create a foreign key; referencing the id variable in the User class,
+    # so that is why it is lowercase u
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __init__(self, title, date, checks, user_id):
+        self.title = title
+        self.date = date
+        self.checks = checks
+        self.user_id = user_id
