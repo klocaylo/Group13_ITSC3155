56c11832d1b04c73247d592b110093e3221ca8f6
Kelsey Locaylocay <klocaylo@uncc.edu>
2020-12-16 03:48:34 -0500

    updated css and favorite feature

--- flask05.py
+++ flask05.py
@@ -352,7 +352,7 @@ def favorite_note(note_id):
             db.session.add(note)
             db.session.commit()
 
-            return redirect(url_for('get_favorite'))
+            return redirect(url_for('get_notes'))
 
         else:
             # GET request - show new note form to edit note
@@ -360,7 +360,37 @@ def favorite_note(note_id):
             # retrieve note from database
             my_note = db.session.query(Note).filter_by(id=note_id).one()
 
-            return render_template('favorite.html', note = my_note, user=session['user'])
+            return render_template('notes.html', note = my_note, user=session['user'])
+    else:
+        # user is not in session redirect to login
+        return redirect(url_for('login'))
+
+@app.route('/notes/unfavorite/<note_id>', methods=['GET', 'POST'])
+def unfavorite_note(note_id):
+
+   #check if a user is saved in session
+    if session.get('user'):
+
+        if request.method == 'POST':
+
+            note = db.session.query(Note).filter_by(id = note_id).one()
+
+            #update note data
+            note.favorite = False
+
+            #update note in DB
+            db.session.add(note)
+            db.session.commit()
+
+            return redirect(url_for('get_notes'))
+
+        else:
+            # GET request - show new note form to edit note
+
+            # retrieve note from database
+            my_note = db.session.query(Note).filter_by(id=note_id).one()
+
+            return render_template('notes.html', note = my_note, user=session['user'])
     else:
         # user is not in session redirect to login
         return redirect(url_for('login'))
